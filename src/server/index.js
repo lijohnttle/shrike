6e677c5956353c5009334b9d2670fb2db4fbddf6
lijohnttle <lijohnttle@outlook.com>
2019-09-29 10:28:16 +0200

    Prepared project

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import config from './config';
+
+const rootPath = path.resolve(__dirname, '../../');
+const app = express();
+
+app.use(express.static(path.resolve(rootPath, 'public')));
+
+app.get('/', (req, res) => {
+    res.setHeader('content-type', 'text/html');
+    res.sendFile(path.resolve(rootPath, 'public/index.html'));
+});
+
+app.listen(config.PORT, () => {
+    console.log(`Server is listening on port ${config.PORT}`);
+});
