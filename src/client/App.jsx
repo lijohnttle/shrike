95a5d06430c27258a5d1dbca028a50c3e9468e92
lijohnttle <lijohnttle@outlook.com>
2019-10-02 22:18:48 +0200

    Linked with GoodReads

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Container, MuiThemeProvider } from '@material-ui/core';
+import { Container, MuiThemeProvider, Box, Typography } from '@material-ui/core';
 import theme from './theme';
 import { Header } from './components/Header';
+import { GoodReadsBookListWidget } from './widgets/GoodReadsBookListWidget';
 
 class App extends React.Component {
     render() {
         return (
             <MuiThemeProvider theme={theme}>
-                <Container>
+                <Box mb={4}>
                     <Header />
+                </Box>
+
+                <Container>
+                    <a name="books">
+                        <Typography variant="h2" gutterBottom={true}>
+                            Books
+                        </Typography>
+                    </a>
+
+                    <Box mb={2}>
+                        <GoodReadsBookListWidget title="Currently Reading" shelf="currently-reading" count={20} />
+                    </Box>
+                    <Box mb={2}>
+                        <GoodReadsBookListWidget title="Read" shelf="read" count={20} />
+                    </Box>
                 </Container>
             </MuiThemeProvider>
         );
