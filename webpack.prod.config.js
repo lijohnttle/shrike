fc095e4994e5e86ca66bb8b5e22e6ae439f21fb6
lijohnttle <lijohnttle@outlook.com>
2019-10-03 21:28:30 +0200

    Added prod build

---
+++ webpack.prod.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.jsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/public',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './public'
+    }
+};
