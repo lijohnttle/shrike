f3ed1d31fcd48896b5fa0ba49dbd80dcb37d638d
Ivan Cherkasov <lijohnttle@outlook.com>
2020-04-07 19:09:24 +0200

    CV: Moved CV data to json

--- src/server/data/repositories/cvRepository.js
+++ src/server/data/repositories/cvRepository.js
@@ -1,61 +1,24 @@
-const cv = {
-    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae orci nec velit elementum ullamcorper id at nunc. Nunc volutpat facilisis ultrices. Morbi porttitor egestas ultricies. Nunc aliquet molestie facilisis. Sed vitae bibendum eros. Nullam dapibus ex tortor, sed commodo elit suscipit at. Nullam eu ultricies lectus. Morbi aliquet laoreet consequat. Morbi dignissim varius ligula a dapibus.',
-    experience: [
-        {
-            position: 'Job Position 3',
-            employer: 'Company',
-            date: '01.01.1001-01.01.1002',
-            location: 'City of Lights, Neverland',
-            accomplishments: [
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                'In quis nisi nec lorem mattis porttitor id eu est.',
-                'Aenean id lectus a erat pretium suscipit.',
-                'Integer nec erat et turpis tempor accumsan.',
-            ],
-            tools: [ 'Tool1', 'Tool2', 'Tool3' ],
-            technologies: [ 'Tech1', 'Tech2', 'Tech3' ]
-        },
-        {
-            position: 'Job Position 2',
-            employer: 'Company',
-            date: '01.01.1001-01.01.1002',
-            location: 'City of Lights, Neverland',
-            accomplishments: [
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                'In quis nisi nec lorem mattis porttitor id eu est.',
-                'Aenean id lectus a erat pretium suscipit.',
-                'Integer nec erat et turpis tempor accumsan.',
-            ],
-            tools: [ 'Tool1', 'Tool2', 'Tool3' ],
-            technologies: [ 'Tech1', 'Tech2', 'Tech3' ]
-        },
-        {
-            position: 'Job Position 1',
-            employer: 'Company',
-            date: '01.01.1001-01.01.1002',
-            location: 'City of Lights, Neverland',
-            accomplishments: [
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                'In quis nisi nec lorem mattis porttitor id eu est.',
-                'Aenean id lectus a erat pretium suscipit.',
-                'Integer nec erat et turpis tempor accumsan.',
-            ],
-            tools: [ 'Tool1', 'Tool2', 'Tool3' ],
-            technologies: [ 'Tech1', 'Tech2', 'Tech3' ]
-        }
-    ],
-    education: [
-        {
-            title: 'Name of the University',
-            description: 'Degree, Description',
-            date: '01.01.1001-01.01.1002',
-            location: 'City of Lights, Neverland'
-        }
-    ]
-};
+import path from 'path';
+import fs from 'fs';
 
 async function getCv() {
-    return await Promise.resolve(cv);
+    const cvDataRaw = await new Promise((resolve, reject) => {
+        const fileName = path.resolve(process.cwd(), `data/cv/cv.json`);
+
+        fs.readFile(fileName, (err, data) => {
+            if (err) {
+                if (process.env.NODE_ENV === 'production') {
+                    reject(new Error('File not found'));
+                }
+                else {
+                    reject(err);
+                }
+            }
+            else resolve(data);
+        })
+    });
+    
+    return JSON.parse(cvDataRaw);
 }
 
 export default {
