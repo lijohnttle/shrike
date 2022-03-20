0ef8d2e8781231fb58a14a55249bfaba24a13c33
Ivan Cherkasov <lijohnttle@outlook.com>
2022-03-20 22:58:01 +0100

    Updated About page

--- src/client/views/About/AboutPage/index.jsx
+++ src/client/views/About/AboutPage/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 import { ContactLink } from '../../../components/ContactLink';
 import { Page } from '../../../components/Page';
 import { Article } from '../../../components/Article';
@@ -11,10 +11,12 @@ import { useStyles } from './styles';
 const AboutPage = () => {
     const classes = useStyles();
     const data = useData();
+
+    const instagramLink = data.contacts.find((contact) => contact.vendor === 'instagram');
     
     return (
         <Page title="About">
-            <Article title="ABOUT ME" updatedOn="February 28, 2022">
+            <Article title="ABOUT ME" updatedOn="March 20, 2022">
                 <ArticleContentBlock className={classes.introductionBlock}>
                     <div className={classes.pictureContainer}>
                         <img className={classes.picture} src="/assets/images/me_large.jpg" />
@@ -26,11 +28,11 @@ const AboutPage = () => {
                         </Typography>
 
                         <Typography align="justify" gutterBottom paragraph>
-                            Since I was a kid, I have been passionate about software development. It started when I first time saw Visual Basic code listings in one of the books my dad bought me when I was a kid, and I got very curious about it at that moment.
+                            Since I was a kid, I have been passionate about software development. It started when I saw Visual Basic code listings for the first time in one of the books my dad bought me when I was a kid. My curiosity took over and now here I am.
                         </Typography>
 
                         <Typography align="justify" gutterBottom paragraph>
-                            My first language that I studied was C++. In 2005 I learned about .NET and C# and decided to give it a try. And now, for the last <b>{new Date().getFullYear() - 2013}+ years of my professional career</b>, they are the main technologies I work with. 
+                            The first language that I studied was C++. When I was 13 I started learning C#, and now, for the last <b>{new Date().getFullYear() - 2013}+ years of my professional career</b>, .NET is the main stack of technologies I work with, but I constantly learn new stuff.
                         </Typography>
 
                         <Typography align="justify" gutterBottom paragraph>
@@ -41,38 +43,28 @@ const AboutPage = () => {
 
                 <ArticleContentBlock maxWidth="md">
                     <Typography variant="h2" align="center" gutterBottom paragraph>
-                        What I'm doing now
+                        What I'm Doing Now
                     </Typography>
 
                     <ul>
                         <li>
                             <Typography align="justify" gutterBottom paragraph>
-                                <b>Living in Wroclaw, Poland.</b> I moved here from Saint Petersburg, Russia in February 2019.
+                                Right now I live in <b>Wroclaw, Poland</b>, but I am about to move to <b>Toronto, Canada.</b>
                             </Typography>
                         </li>
                         <li>
                             <Typography align="justify" gutterBottom paragraph>
-                                <b>Working as a Senior Software Engineer</b> in a Swiss multinational bank.
+                                Working as a <b>Senior Software Engineer</b> in a Swiss multinational bank.
                             </Typography>
                         </li>
                         <li>
                             <Typography align="justify" gutterBottom paragraph>
-                                <b>Investing and trading securities.</b>
+                                Getting ready to get married.
                             </Typography>
                         </li>
                         <li>
                             <Typography align="justify" gutterBottom paragraph>
-                                <b>Travelling.</b>
-                            </Typography>
-                        </li>
-                        <li>
-                            <Typography align="justify" gutterBottom paragraph>
-                                <b>Working out.</b> "Mens sana in corpore sano" ("A healthy mind in a healthy body").
-                            </Typography>
-                        </li>
-                        <li>
-                            <Typography align="justify" gutterBottom paragraph>
-                                <b>Striving for self-improvement.</b>
+                                Learning Zulu and French.
                             </Typography>
                         </li>
                     </ul>
@@ -80,18 +72,33 @@ const AboutPage = () => {
 
                 <ArticleContentBlock maxWidth="md">
                     <Typography variant="h2" align="center" gutterBottom paragraph>
-                        My plans
+                        Interests &amp; Hobbies
                     </Typography>
 
                     <ul>
                         <li>
                             <Typography align="justify" gutterBottom paragraph>
-                                <b>Relocate to Canada.</b> Currently I am in the process of moving to Canada.
+                                <Link href={instagramLink.value} target="_blank">Travel.</Link>
                             </Typography>
                         </li>
                         <li>
                             <Typography align="justify" gutterBottom paragraph>
-                                <b>Get married.</b> We are engaged.
+                                Finance, investing and trading.
+                            </Typography>
+                        </li>
+                        <li>
+                            <Typography align="justify" gutterBottom paragraph>
+                                Fitness.
+                            </Typography>
+                        </li>
+                        <li>
+                            <Typography align="justify" gutterBottom paragraph>
+                                Self-Improvement.
+                            </Typography>
+                        </li>
+                        <li>
+                            <Typography align="justify" gutterBottom paragraph>
+                                Learning languages. I speak Russian (native), English (fluent), French (beginner), Zulu (beginner).
                             </Typography>
                         </li>
                     </ul>
