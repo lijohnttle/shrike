28ada7b087970ecf541b3837000c8ab612b1c8e4
Ivan Cherkasov <lijohnttle@outlook.com>
2022-04-22 14:22:19 +0200

    Add logs

--- src/server/index.js
+++ src/server/index.js
@@ -32,9 +32,14 @@ async function main() {
 
     app.use(cors());
 
-    // app.get('/', (req, res) => {
-    //     res.sendFile()
-    // });
+    app.get('/', (_, res) => {
+        console.log("Index file path: " + path.resolve(appContext.rootPath, 'dist/public/index.html'));
+
+        res.setHeader('content-type', 'text/html');
+        res.setHeader('access-control-allow-origin', '*');
+    
+        res.sendFile(path.resolve(appContext.rootPath, 'dist/public/index.html'));
+    });
 
     registerControllers(app, {
         rootPath: process.cwd()
